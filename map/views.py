07b74dc9d16b84419ce006f81877a24c01c3026f
gjeremy29 <gjeremy0229@outlook.es.com>
2024-10-22 14:15:11 -0500

    test03

--- map/views.py
+++ map/views.py
@@ -1,7 +1,9 @@
-# map/views.py
-
 from django.shortcuts import render
+import folium
+
 
 def map(request):
-    # Logic for your map view goes here
-    return render(request, 'map.html')
+    initialMap = folium.Map(location=[-34.6460675,-58.5268284], zoom_start=11)
+    context = {'map':initialMap._repr_html_()}
+
+    return render(request, 'map.html', context)
