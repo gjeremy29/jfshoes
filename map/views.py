6f3cfec1444bef51ef6e52f255b651080d3cfedf
gjeremy29 <gjeremy0229@outlook.es.com>
2024-10-23 12:03:16 -0500

    endfolium

--- map/views.py
+++ map/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from .models import Location
 import folium
-
+from folium.plugins import FastMarkerCluster
 
 def map(request):
-    initialMap = folium.Map(location=[-34.6460675,-58.5268284], zoom_start=11)
-    context = {'map':initialMap._repr_html_()}
+    locations = Location.objects.all()
+    initialMap = folium.Map(location=[-2.1919108,-79.8854934], zoom_start=7)
+    # Creamos el Clustering de los marcadores
+    #latitudes = [location.lat for location in locations]
+    #longitudes = [location.lng for location in locations]
+    #popups = [location.name for location in locations]
 
-    return render(request, 'map.html', context)
+    #print(latitudes)
+    #print(longitudes)
+    #print(list(zip(latitudes, longitudes, popups)))
+
+    #FastMarkerCluster(data=list(zip(latitudes, longitudes, popups))).add_to(initialMap)
+
+    #Creamos los marcadores
+    for location in locations:
+        coordinates = (location.lat, location.lng)
+        folium.Marker(coordinates, popup='Sucursal ' + location.name).add_to(initialMap)
+
+    context = {'map':initialMap._repr_html_(), 'locations':locations}
+
+    return render(request, 'map/map.html', context)
