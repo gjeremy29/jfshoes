b3290d22a74bc46e16651ecca90a3f376ba09f72
gjeremy29 <gjeremy0229@outlook.es.com>
2024-10-24 13:47:43 -0500

    change5

--- jsShoesW/settings.py
+++ jsShoesW/settings.py
@@ -86,10 +86,7 @@ WSGI_APPLICATION = 'jsShoesW.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(defaul=os.getenv('DATABASE_URL'))
 }
 
 
