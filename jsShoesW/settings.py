bb3b582a7caa19b85344d7c5a5a987180f4d9161
gjeremy29 <gjeremy0229@outlook.es.com>
2024-11-21 14:45:43 -0500

    newshost

--- jsShoesW/settings.py
+++ jsShoesW/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-q-k&*4po0p0=@6g1o^4=p5*k+8(^)=si6wz&__d60u#(_elfr6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-c80e.up.railway.app']
+ALLOWED_HOSTS = ['web-production-c80e.up.railway.app','jfshoes.net','www.jfshoes.net']
 
 
 # Application definition
